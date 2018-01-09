2ec66f75cac2a12f0c646cbb008bf4cafef5ff36
Hacı Kanal <haci.kanal05@gmail.com>
2018-01-09 10:26:41 +0300

    Update TestAdapter.java

--- TestAdapter.java
+++ TestAdapter.java
@@ -1,13 +1,16 @@
 public class TestAdapter extends HKBaseRecyclerAdapter<TestItem> {
 
+    private final int NAME_VIEW = R.id.name_view;
+    private final int AGE_VIEW = R.id.age_view;
+    
     @Override
     protected int[] setClickableChilds() {
-        return new int[]{R.id.name_view};
+        return new int[]{NAME_VIEW};
     }
 
     @Override
     protected int[] setChildsToInflate() {
-        return new int[]{R.id.age_view, R.id.name_view};
+        return new int[]{NAME_VIEW, AGE_VIEW};
     }
 
     @Override
@@ -17,8 +20,8 @@ public class TestAdapter extends HKBaseRecyclerAdapter<TestItem> {
 
     @Override
     protected void onBind(TestItem item, HKViewHolder viewHolder) {
-        TextView nameView = (TextView) viewHolder.getView(R.id.name_view);
-        TextView ageView = (TextView) viewHolder.getView(R.id.age_view);
+        TextView nameView = (TextView) viewHolder.getView(NAME_VIEW);
+        TextView ageView = (TextView) viewHolder.getView(AGE_VIEW);
 
         if (item != null) {
             nameView.setText(item.getName());
